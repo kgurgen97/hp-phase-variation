3a63dc320067bead23ea2d573d28f2d3a7662222
Gurgen Karapetyan <karagurgen23@gmail.com>
2026-09-23 21:18:37 +0400

    Rename caller package references

--- src/hp_phasevar/__init__.py
+++ src/hp_phasevar/__init__.py
@@ -1,2 +1,2 @@
-"""g5caller: repeat-aware spanning-read caller for phase-variable loci (pure stdlib)."""
+"""hp_phasevar: repeat-aware spanning-read caller for phase-variable loci (pure stdlib)."""
 __version__ = "0.1.0"
